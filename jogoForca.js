eadd1017a9713a592345b8fec214bb162c483ebd
nei <nei.thomass@gmail.com>
2021-03-08 11:57:38 -0300

    add naimations css

--- jogoForca.js
+++ jogoForca.js
@@ -1,7 +1,10 @@
 /*
 / Jogo da força
-/ file: jogoForca.js
-/ 
+/ file: ./jogoForca.js
+/ description: arquivo que contém toda lógica do jogo
+/ date: março/2021
+/ author: Nei Thomassin Dutra
+/ git: https://gitlab.com/neithomass/hangman_game
 */
 
 var palavra = '';
@@ -10,6 +13,9 @@ var count = 0;
 var winn = 0;
 var letra_d = [];
 var l_digitada = [];
+var dica_i = '';
+var dica_ii = '';
+var dica_iii = '';
 
 // chama a função criaCaixaDeLetra(palavra) ao receber valor por input "in_word"
 function criaCaixaDeLetra() {
@@ -25,10 +31,12 @@ function criaCaixaDeLetra() {
         l_digitada = '';
         document.getElementById('in_word').value = '';
         document.getElementById('in_word').focus();
+        return;
     }
     document.getElementById('palavra').style.display = 'none';
-    document.getElementById('letra').style.display = 'block';
-    document.getElementById('div_word').innerHTML += '<p class="text-primary ml-5">Palavra com '+palavra.length+' letras.</p>';
+    document.getElementById('input_dica_i').style.display = 'block';
+    document.getElementById('dica_i').focus();
+    document.getElementById('div_word').innerHTML += '<p class="text-primary ml-3">Palavra com '+palavra.length+' letras.</p>';
     
     for(let w = 0; w < palavra.length; w++) {
     
@@ -36,6 +44,43 @@ function criaCaixaDeLetra() {
     }
     
     document.getElementById('in_word').value = '';
+}
+
+// chama a função guardaDica(dica) ao receber valor por input "dica_i", "dica_ii" e "dica_iii"
+function guardaDica(d) {
+
+
+    input_i = document.getElementById('input_dica_i');
+    input_ii = document.getElementById('input_dica_ii');
+    input_iii = document.getElementById('input_dica_iii');
+    if(d === 'dica_i') {
+    
+        dica_i = document.getElementById('dica_i').value;
+        input_i.style.display = 'none';
+        input_ii.style.display = 'block';
+        document.getElementById('dica_ii').focus();
+        document.getElementById('dica_i').value = '';
+        return;
+    }
+    if(d === 'dica_ii') {
+    
+        dica_ii = document.getElementById('dica_ii').value;
+        input_ii.style.display = 'none';
+        input_iii.style.display = 'block';
+        document.getElementById('dica_iii').focus();
+        document.getElementById('dica_ii').value = '';
+        return;
+    }
+    if(d === 'dica_iii') {
+    
+        dica_iii = document.getElementById('dica_iii').value;
+        input_iii.style.display = 'none';
+        document.getElementById('letra').style.display = 'block';
+        document.getElementById('letra').focus();
+        document.getElementById('dica_iii').value = '';
+        return;
+    }
+    
 }
 
 // chama a função testtaLetra(letra) ao receber  valor por input "in_letter"
@@ -47,6 +92,7 @@ function testaLetra() {
         letra = '';
         document.getElementById('in_letter').value = '';
         document.getElementById('in_letter').focus();
+        return;
     }
     
     letra_d += letra;
@@ -79,6 +125,18 @@ function testaLetra() {
             count ++;
             document.getElementById('forca'+count).style.display = 'block';
             document.getElementById('forca'+count).classList.toggle('animate');
+            
+            if(count == 2) {
+            
+                document.getElementById('dica').style.display = 'block';
+                document.getElementById('dica').innerHTML += '<p class="text-warning ml-3"><strong>1/3: </strong>'+dica_i+'...</p>';
+            }if(count == 4) {
+            
+                document.getElementById('dica').innerHTML += '<p class="text-warning ml-3"><strong>2/3: </strong>'+dica_ii+'...</p>';
+            }if(count == 6) {
+            
+                document.getElementById('dica').innerHTML += '<p class="text-warning ml-3"><strong>3/3: </strong>'+dica_iii+'...</p>';
+            }
             if(count == 7) {
                 setTimeout( () => {
                 
@@ -86,7 +144,6 @@ function testaLetra() {
                 }, 500);
                 setTimeout( function() {
                 
-                    alert('Acabou o jogo, o boneco está na forca!!!');
                     palavra = '';
                     letra = '';
                     count = 0;
