c2704061f870193d6feb437560bdbd29c1498a14
nei <nei.thomass@gmail.com>
2021-03-15 06:07:46 -0300

    new features with javascript

--- jogoForca.js
+++ jogoForca.js
@@ -7,17 +7,23 @@
 / git: https://gitlab.com/neithomass/hangman_game
 */
 
+// declaração das variáveis 
+
 var palavra = '';
 var letra = '';
 var count = 0;
 var win = 0;
 var letra_d = [];
 var l_digitada = [];
+var plvr = '';
 var dica_i = '';
 var dica_ii = '';
 var dica_iii = '';
+var testNumRand = [];
 
-// chama a função que "randomiza" palavra e dicas
+// chama a função que escolhe aleatoriamente palavra e dicas,
+// faz a busca em dados no formato json no arquivo "palavraoculta.json",
+// usando a classe "XMLHttpRequest"...
 
 function random() {
     
@@ -33,13 +39,28 @@ function random() {
     
         var res = request.response;
         var d = res.data;
+        // passa o array para a função que escolhe palavra e dicas
         saveOnVar(d);
+        // passa a palavra escolhida para a função que cria as caixas do jogo no html
         criaCaixaDeLetra(plvr);
     }
+    // função que faz a escolha aleatoria e guarda os valores nas variáveis
     function saveOnVar(data) {
-    
+            
         let range = data.length;
-        let numRand = Math.floor(Math.random() * range);
+        var numRand = Math.floor(Math.random() * range);
+
+        for(let p = 0; p < testNumRand.length; p++) {
+
+            if(numRand === testNumRand[p]) {
+
+                return;
+            }
+            else {
+                
+                testNumRand += numRand;
+            }
+        }
         
         for(let i = 0; i < range; i++) {
         
@@ -54,7 +75,9 @@ function random() {
     }
 }
 
-// chama a função criaCaixaDeLetra(palavra) ao receber valor por input "in_word"
+// chama a função criaCaixaDeLetra(palavra) ao receber valor por input ("in_word") ou,
+// pela função "random()".
+
 function criaCaixaDeLetra(p) {
 
     if(p !== 'yes') {
@@ -68,12 +91,6 @@ function criaCaixaDeLetra(p) {
         if(palavra == '') {
         
             alert('Digite uma palavra ou deixe o sistema escolher!');
-            window.location.reload(true);
-            palavra = '';
-            letra = '';
-            count = 0;
-            winn = 0;
-            l_digitada = '';
             document.getElementById('in_word').value = '';
             document.getElementById('in_word').focus();
             return;
@@ -82,10 +99,10 @@ function criaCaixaDeLetra(p) {
         document.getElementById('palavra').style.display = 'none';
         document.getElementById('input_dica_i').style.display = 'block';
         document.getElementById('dica_i').focus();
-        document.getElementById('div_word').innerHTML += '<p class="text-primary ml-3">Palavra com '+palavra.length+' letras.</p>';
     
     }
-    
+    document.getElementById('div_word').innerHTML += '<p class="text-primary ml-3">Palavra com '+palavra.length+' letras.</p>';
+    // laço que cria as caixas/posições no html
     for(let w = 0; w < palavra.length; w++) {
     
         document.getElementById('div_word').innerHTML += '<div class="letter text-center slide" id="div_l_'+w+'"></div>';
@@ -94,7 +111,9 @@ function criaCaixaDeLetra(p) {
     document.getElementById('in_word').value = '';
 }
 
-// chama a função guardaDica(dica) ao receber valor por input "dica_i", "dica_ii" e "dica_iii"
+// chama a função guardaDica(dica) ao receber valor por input ("dica_i", "dica_ii" e "dica_iii")
+// só é usada caso não seja escolha automática aleatoria (random())
+
 function guardaDica(d) {
 
 
@@ -105,38 +124,49 @@ function guardaDica(d) {
     switch(d) {
     
         case 'dica_i':
+
             dica_i = document.getElementById('dica_i').value;
             input_i.style.display = 'none';
             input_ii.style.display = 'block';
             document.getElementById('dica_i').value = '';
             document.getElementById('dica_ii').focus();
             break;
+
         case 'dica_ii':
+
             dica_ii = document.getElementById('dica_ii').value;
             input_ii.style.display = 'none';
             input_iii.style.display = 'block';
             document.getElementById('dica_ii').value = '';
             document.getElementById('dica_iii').focus();
             break;
+
         case 'dica_iii':
+
             dica_iii = document.getElementById('dica_iii').value;
             input_iii.style.display = 'none';
             document.getElementById('letra').style.display = 'block';
             document.getElementById('dica_iii').value = '';
             document.getElementById('letra').focus();
             break;
+
         default:
+
             alert('[ERROR] This error is not defined!')
     }
 }
 
-// chama a função testaLetra(letra) ao receber  valor por input "in_letter"
+// chama a função testaLetra() ao receber  valor por input ("in_letter")
+// função principal do jogo que, percorre o objeto palavra e verifica se contêm a letra
+// quando encontra imprime a caixa/posição correspondente,
+// a cada dois erros imprime uma das três dicas e
+// a cada erro monta o boneco na forca :(
+
 function testaLetra() {
     
     letra = document.getElementById('in_letter').value.toUpperCase();
     document.getElementById('in_letter').value = '';
     document.getElementById('in_letter').focus();
-    console.log(palavra); 
     
     if(letra == '' || letra.length > 1) {
     
@@ -147,6 +177,7 @@ function testaLetra() {
     
     letra_d += letra;
     var print = letra_d.split('').join(' - ','');
+    // após adicionar a letra na array ("letra_d") imprime o html com as palavras já digitadas
     document.getElementById('l_digitada').innerHTML = '<h4 class="text-danger text-center bg-white">'+print+'</h4>';
 
     if((!l_digitada.includes(letra)) || (l_digitada.indexOf(letra) === -1)) {
@@ -154,36 +185,32 @@ function testaLetra() {
         l_digitada += letra;
     
         if(palavra.includes(letra)) {
-        
+            // laço que verifica a posição da letra na palvra
             for(let i = 0; i < palavra.length; i++) {
             
                 if(letra === palavra[i]){
-                
+                    // imprime a letra no html na posição/caixa correspondente 
                     document.getElementById('div_l_'+i).innerHTML = '<h3 class="font-weight-bold">'+letra+'</h3>';
                     win ++;
-                    
+                    // se o contador ("win") atingiu o tamanho da palavra o jogador ganhou :)
                     if(win === palavra.length) {
             
-                        setTimeout( () => {
                             document.getElementById('game_over').style.display = 'block';
-                            document.getElementById('game_over').innerHTML = '<h1 class="text-danger">YOU WON!!! CONGRATULATIONS!!!</h1>';
-                        }, 500);
+                            document.getElementById('title-final').innerText = 'YOU WON!!! CONGRATULATIONS!!!';
                         
-                        setTimeout( function() {
                             palavra = '';
                             letra = '';
                             count = 0;
                             winn = 0;
                             l_digitada = '';
-                            window.location.reload(true);
-                        }, 4000);
-                        
                     }
                 }
             }
         }
         else{
-        
+            // este bloco trata das letras que não estão na palavra do jogo
+            // monta parte do boneco e,
+            // de acordo com o contador ("count") imprime no html uma dica a cada dois erros
             count ++;
             document.getElementById('forca'+count).style.display = 'block';
             document.getElementById('forca'+count).classList.toggle('animate');
@@ -204,22 +231,18 @@ function testaLetra() {
                 document.getElementById('dica').innerHTML += '<p class="text-warning inidica"><strong>(3/3): </strong>'+dica_iii+'...</p>';
             
             }
+            // se o contador ("count") chegou ao tamanho das partes do boneco
+            // o jogo termina e o jogador perdeu o jogo :(
             if(count == 7) {
             
-                setTimeout( () => {
                     document.getElementById('game_over').style.display = 'block';
-                    document.getElementById('game_over').innerHTML = '<h1 class="text-danger">GAME OVER</h1>';
-                }, 500);
+                    document.getElementById('title-final').innerText = 'YOU LOST!!! GAME OVER!!!';
                 
-                setTimeout( function() {
                     palavra = '';
                     letra = '';
                     count = 0;
                     winn = 0;
                     l_digitada = '';
-                    window.location.reload(true);
-                }, 4000);
-
             }
         }
         
@@ -228,9 +251,31 @@ function testaLetra() {
         
     }
     else {
-    
+        // se a letra já foi digitada abre janela de aviso
         alert('A letra '+letra+' já foi digitada!!');
     }
 }
 
+// função que reseta o jogo mantendo alguns valores da jogada anterior
 
+function resetGame() {
+
+    palavra = '';
+    letra = '';
+    count = 0;
+    winn = 0;
+    l_digitada = '';
+    window.location.reload(true);
+}
+
+// função que força o recarregamento da página ("deprecated")
+
+function exitGame() {
+
+    palavra = '';
+    letra = '';
+    count = 0;
+    winn = 0;
+    l_digitada = '';
+    window.location.reload(true);
+}
