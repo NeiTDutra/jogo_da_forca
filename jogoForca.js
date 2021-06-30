698539bee21bb24d8ed6daa5a276635771872030
nei <nei.thomass@gmail.com>
2021-06-30 20:47:26 -0300

    desenvolvendo continuação do jogo

--- jogoForca.js
+++ jogoForca.js
@@ -7,9 +7,10 @@
 / git: https://gitlab.com/neithomass/hangman_game
 */
 
-// declaração das variáveis 
+// declaração das variáveis
 
 var palavra = '';
+var plv_anterior = [];
 var letra = '';
 var count = 0;
 var win = 0;
@@ -26,20 +27,20 @@ var testNumRand = [];
 // usando a classe "XMLHttpRequest"...
 
 function random() {
-    
+
     document.getElementById('palavra').style.display = 'none';
     document.getElementById('letra').style.display = 'block';
     document.getElementById('letra').focus();
-    
+
     var requestUrl = './palavraoculta.json';
     var request = new XMLHttpRequest();
-    
+
     request.open('POST', requestUrl, true);
     request.responseType = 'json';
     request.send(null);
     request.onload = () => {
         if(request.readyState === 4) {
-        
+
             var res = request.response;
             var d = res.data;
         }
@@ -50,7 +51,7 @@ function random() {
     }
     // função que faz a escolha aleatoria e guarda os valores nas variáveis
     function saveOnVar(data) {
-            
+
         let range = data.length;
         var numRand = Math.floor(Math.random() * range);
 
@@ -61,20 +62,31 @@ function random() {
                 return;
             }
             else {
-                
+
                 testNumRand += numRand;
             }
         }
-        
+
         for(let i = 0; i < range; i++) {
-        
+
             if(i === numRand) {
-            
-                plvr = data[i].palavra;
-                dica_i = data[i].dica.dica_i;
-                dica_ii = data[i].dica.dica_ii;
-                dica_iii = data[i].dica.dica_iii;
+/*
+                forEach((plv_anterior, i) => {
+                  if(i != null && i === data[i].palavra) {
+                    random();
+                    return;
+                  }
+                  else {
+*/
+                    plvr = data[i].palavra;
+                    dica_i = data[i].dica.dica_i;
+                    dica_ii = data[i].dica.dica_ii;
+                    dica_iii = data[i].dica.dica_iii;
+                  }
+/*
+                });
             }
+*/
         }
     }
 }
@@ -85,34 +97,34 @@ function random() {
 function criaCaixaDeLetra(p) {
 
     if(p !== 'yes') {
-    
+
         palavra = p.toUpperCase();
     }
     else {
-    
+
         palavra = document.getElementById('in_word').value.toUpperCase();
-    
+
         if(palavra == '') {
-        
+
             alert('Digite uma palavra ou deixe o sistema escolher!');
             document.getElementById('in_word').value = '';
             document.getElementById('in_word').focus();
-            
+
             return;
         }
-        
+
         document.getElementById('palavra').style.display = 'none';
         document.getElementById('input_dica_i').style.display = 'block';
         document.getElementById('dica_i').focus();
-    
+
     }
     document.getElementById('div_word').innerHTML += '<p class="text-primary ml-3">Palavra com '+palavra.length+' letras.</p>';
     // laço que cria as caixas/posições no html
     for(let w = 0; w < palavra.length; w++) {
-    
+
         document.getElementById('div_word').innerHTML += '<div class="letter text-center slide" id="div_l_'+w+'"></div>';
     }
-    
+
     document.getElementById('in_word').value = '';
 }
 
@@ -125,9 +137,9 @@ function guardaDica(d) {
     input_i = document.getElementById('input_dica_i');
     input_ii = document.getElementById('input_dica_ii');
     input_iii = document.getElementById('input_dica_iii');
-    
+
     switch(d) {
-    
+
         case 'dica_i':
 
             dica_i = document.getElementById('dica_i').value;
@@ -168,41 +180,41 @@ function guardaDica(d) {
 // a cada erro monta o boneco na forca :(
 
 function testaLetra() {
-    
+
     letra = document.getElementById('in_letter').value.toUpperCase();
     document.getElementById('in_letter').value = '';
     document.getElementById('in_letter').focus();
-    
+
     if(letra == '' || letra.length > 1) {
-    
+
         alert('Digite uma e, somente uma letra!');
         letra = '';
         return;
     }
-    
+
     letra_d += letra;
     var print = letra_d.split('').join(' - ','');
     // após adicionar a letra na array ("letra_d") imprime o html com as palavras já digitadas
     document.getElementById('l_digitada').innerHTML = '<h4 class="text-danger text-center bg-white">'+print+'</h4>';
 
     if((!l_digitada.includes(letra)) || (l_digitada.indexOf(letra) === -1)) {
-    
+
         l_digitada += letra;
-    
+
         if(palavra.includes(letra)) {
             // laço que verifica a posição da letra na palvra
             for(let i = 0; i < palavra.length; i++) {
-            
+
                 if(letra === palavra[i]){
-                    // imprime a letra no html na posição/caixa correspondente 
+                    // imprime a letra no html na posição/caixa correspondente
                     document.getElementById('div_l_'+i).innerHTML = '<h3 class="font-weight-bold">'+letra+'</h3>';
                     win ++;
                     // se o contador ("win") atingiu o tamanho da palavra o jogador ganhou :)
                     if(win === palavra.length) {
-            
+
                             document.getElementById('game_over').style.display = 'block';
                             document.getElementById('title-final').innerText = 'YOU WON!!! CONGRATULATIONS!!!';
-                        
+
                             palavra = '';
                             letra = '';
                             count = 0;
@@ -219,30 +231,30 @@ function testaLetra() {
             count ++;
             document.getElementById('forca'+count).style.display = 'block';
             document.getElementById('forca'+count).classList.toggle('animate');
-            
+
             if(count == 2) {
-            
+
                 document.getElementById('dica').style.display = 'block';
                 setTimeout( () => {
                     document.getElementById('dica').innerHTML += '<p class="text-warning inidica"><strong>(1/3): </strong>'+dica_i+'...</p>';
                 }, 1000);
 
             }if(count == 4) {
-            
+
                 document.getElementById('dica').innerHTML += '<p class="text-warning inidica"><strong>(2/3): </strong>'+dica_ii+'...</p>';
-            
+
             }if(count == 6) {
-            
+
                 document.getElementById('dica').innerHTML += '<p class="text-warning inidica"><strong>(3/3): </strong>'+dica_iii+'...</p>';
-            
+
             }
             // se o contador ("count") chegou ao tamanho das partes do boneco
             // o jogo termina e o jogador perdeu o jogo :(
             if(count == 7) {
-            
+
                     document.getElementById('game_over').style.display = 'block';
                     document.getElementById('title-final').innerText = 'YOU LOST!!! GAME OVER!!!';
-                
+
                     palavra = '';
                     letra = '';
                     count = 0;
@@ -250,10 +262,10 @@ function testaLetra() {
                     l_digitada = '';
             }
         }
-        
+
         letra = '';
-        
-        
+
+
     }
     else {
         // se a letra já foi digitada abre janela de aviso
@@ -261,16 +273,32 @@ function testaLetra() {
     }
 }
 
-// função que reseta o jogo mantendo alguns valores da jogada anterior
+// função que isola a palavra do jogo para não
+// ser usada novamente caso o jogador queira
+// continuar jogando *em desenvolvimento
 
-function resetGame() {
-
+function jogarNovamente() {
+    plv_anterior += palavra;
     palavra = '';
     letra = '';
     count = 0;
     winn = 0;
-    l_digitada = '';
-    window.location.reload(true);
+    letra_d = [];
+    l_digitada = [];
+    plvr = '';
+    dica_i = '';
+    ica_ii = '';
+    dica_iii = '';
+    testNumRand = [];
+    requestUrl = '';
+    request = '';
+    res = '';
+    d = '';
+    range = '';
+    numRand = '';
+    //window.location.reload(true);
+    document.getElementById('game_over').style.display = 'none';
+    random();
 }
 
 // função que força o recarregamento da página ("deprecated")
